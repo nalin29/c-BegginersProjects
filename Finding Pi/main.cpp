8e0b766d5fc17a0db8473e26845637fc3550d9ca
nalin29 <nalinmahajan@outlook.com>
2018-06-19 23:39:38 -0500

    2nd

---
+++ Finding Pi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  decimalPi
+//
+//  Created by Nalin Mahajan on 6/19/18.
+//  Copyright © 2018 Nalin Mahajan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!";
+    return 0;
+}
